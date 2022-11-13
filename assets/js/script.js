85d75fb35448d03b3a7c6e53bda1a6d5a52c15e5
Omar Aladheem <omaraladheem@gmail.com>
2022-11-13 19:49:33 +0000

    Set up structure of game funtions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongQuestion() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}

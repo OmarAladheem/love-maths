c439c371e21ff9c5a83ef66e5661cf23ef914aeb
Omar Aladheem <omaraladheem@gmail.com>
2022-11-13 21:15:42 +0000

    Add code to calculate two random unmbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,15 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 })
+/**
+ * The manin game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
 
+    // Creates two random unmbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
@@ -23,6 +30,7 @@ function checkAnswer() {
 function calculateCorrectAnswer() {
     
 }
+
 
 function incrementScore() {
     
